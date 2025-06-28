e85f4a29a768b8d5863e0627899559067e5d5e99
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-06-28 09:06:13 +0100

    add function for creating card and displaying books

--- script.js
+++ script.js
@@ -18,5 +18,30 @@ function addBookToLibary(title,author,num_of_pages,read) {
     
 }
 
+function createCard(book){
+    let container = document.querySelector('.container');
+    let card = document.createElement('div')
+    card.classList.add("card");
+    let h4 = document.createElement('h4');
+    h4.textContent = book.title;
+    let p = document.createElement("p");
+    p.textContent = book.info();
+    card.appendChild(h4);
+    card.appendChild(p);
+    container.appendChild(card);
+}
+
+function displayBook(arr){
+    for (const book of arr){
+        createCard(book);
+    }
+}
+
 addBookToLibary("Yellow Face", "R.F.Kuang", 450, "Yes");
-console.log(myLibary)
+addBookToLibary("The Palace of Illusions","Chitra Banerjee", 360, "Not yet");
+addBookToLibary("The White Tiger", "Arvind Adiga", 276, "Yes");
+addBookToLibary("The Reaper and Angels", "Alden Bell", 205, "Not yet");
+addBookToLibary("Life of Pi","Yann Martel", 460, "Yes");
+addBookToLibary("Marmalade", "J.F Bob", 400, "Yes");
+addBookToLibary("The curious incident of the dog in the night-time", "Mark Haddon", 226, "Yes")
+displayBook(myLibary);
