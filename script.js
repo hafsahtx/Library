03b555e60cc0f9656dd70882fa73f1f62bd21c20
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-06-29 10:37:58 +0100

    Sepate toggle-wrapper into a different function

--- script.js
+++ script.js
@@ -27,33 +27,27 @@ function addBookToLibary(title,author,num_of_pages,read) {
     
 }
 
-
-
-function createCard(book){
-    let container = document.querySelector('.container');
-    let card = document.createElement('div')
-    card.classList.add("card");
-    card.setAttribute("data-book-id",book.book_id);
-    let h4 = document.createElement('h4');
-    h4.textContent = book.title;
-    let p = document.createElement("p");
-    p.textContent = book.info();
-    const div = document.createElement("div");
-    div.classList.add("wrapper");
+function createToggleWrapper(book,p){
+    //toggle wrapper
     const toggle_wrapper = document.createElement("div");
     toggle_wrapper.classList.add("toggle-wrapper");
+    toggle_wrapper.setAttribute("data-book-id",book.book_id);
+    toggle_wrapper.setAttribute("data-role","toggle");
+    //label
     const label = document.createElement("label");
     label.classList.add("switch");
-    const input = document.createElement("input");
+    //span
     const span = document.createElement("span");
     span.classList.add("slider", "round");
+    //spantext
     const spantext = document.createElement("span");
     spantext.classList.add("label-text");
     spantext.innerHTML = "Not Read";
+    //input
+    const input = document.createElement("input");
     input.setAttribute("type","checkbox"); 
     input.setAttribute("id","toggleSwitch");
-    label.appendChild(input);
-    label.appendChild(span);   
+    //toggles a book instance’s read status
     if(book.read === "Read"){
         input.checked = true;
         spantext.innerHTML = "Read";
@@ -68,7 +62,6 @@ function createCard(book){
             status = true;
             spantext.innerHTML = "Read";
             book.book_status(status);
-            console.table(book);
             p.textContent = book.info();
             
         }else{
@@ -76,33 +69,59 @@ function createCard(book){
             spantext.innerHTML = "Not read";
             book.book_status(status);
             p.textContent = book.info();
-            console.table(book);
 
         }
         
     });
+    //add to DOM
+    label.appendChild(input);
+    label.appendChild(span);   
+    toggle_wrapper.appendChild(label);
+    toggle_wrapper.appendChild(spantext);
+    return toggle_wrapper;
+}
+
+
+function createCard(book){
+    let container = document.querySelector('.container');
+    let top_wrapper = document.createElement("div");
+    top_wrapper.classList.add("top-wrapper");
+    let card = document.createElement('div')
+    card.classList.add("card");
+    card.setAttribute("data-book-id",book.book_id);
+    let h4 = document.createElement('h4');
+    h4.textContent = book.title;
+    let p = document.createElement("p");
+    p.textContent = book.info();
+    const div = document.createElement("div");
+    div.classList.add("wrapper");
+    let toggle_wrapper = createToggleWrapper(book,p); //function call
+    //create remove button
     let removeBtn = document.createElement("button");
-    removeBtn.textContent = "Remove Book";
+    removeBtn.textContent = "Remove";
     removeBtn.classList.add("remove");
     removeBtn.setAttribute("data-book-id",book.book_id);
+    //remove button pressed
     removeBtn.addEventListener("click",()=>{
+    //link card and button with dataset
     let data = removeBtn.dataset.bookId;
     let card = document.querySelector(`[data-book-id = "${data}"]`);
     const container = document.querySelector(".container");
     container.removeChild(card);
     let index = 0;
     for (let book in myLibary){
+        //remove from libaray
         if (book.book_id == data){
             myLibary.splice(index, 1);
         }
         index++;
     }});
-    toggle_wrapper.appendChild(label);
-    toggle_wrapper.appendChild(spantext);
+    //add elements to DOM
     div.appendChild(toggle_wrapper);
     div.appendChild(removeBtn);
-    card.appendChild(h4);
-    card.appendChild(p);
+    top_wrapper.append(h4);
+    top_wrapper.appendChild(p);
+    card.appendChild(top_wrapper);
     card.appendChild(div);
     container.appendChild(card);
 }
@@ -133,15 +152,18 @@ function showDialog(){
     const cancel = document.getElementById("cancel");
     const form = document.getElementById("myForm");
     showBtn.addEventListener("click",()=>{
+        //show dialog is add book button pressed
         dialog.showModal();
     });
     submit.addEventListener("click", (e)=>{
+        //close dialog and add new book
         e.preventDefault();
         addNewBook();
         form.reset();
         dialog.close();
     });
     cancel.addEventListener("click",(e)=>{
+        //close dialog
         e.preventDefault();
         form.reset();
         dialog.close();
@@ -156,7 +178,6 @@ addBookToLibary("The Palace of Illusions","Chitra Banerjee", 360, "Not read");
 addBookToLibary("The White Tiger", "Arvind Adiga", 276, "Read");
 addBookToLibary("The Reaper and Angels", "Alden Bell", 205, "Not read");
 addBookToLibary("Life of Pi","Yann Martel", 460, "Read");
-addBookToLibary("Marmalade", "J.F Bob", 400, "Not Read");
 addBookToLibary("The curious incident of the dog in the night-time", "Mark Haddon", 226, "Read")
 displayBook(myLibary);
 showDialog();
